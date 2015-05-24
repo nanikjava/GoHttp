304083541bbe773cc5b652bffb95a7e12a00cde5
Nanik Tolaram <nanikjava@gmail.com>
2015-05-24 18:23:38 +1000

    Remove unneeded LOCAL_LDLIBS for compilation

--- Android.mk
+++ Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES :=  GoHttp.c
-LOCAL_LDLIBS += -lrt -ldl -lpthread -llog
 LOCAL_CFLAGS := -DDEBUG_ANDROID
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_MODULE := gohttp
